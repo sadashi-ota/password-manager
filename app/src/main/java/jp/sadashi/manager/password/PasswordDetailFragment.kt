bcb608f558a8197a5288a15123f4b39bf1c81fba
Sadashi Ota <sadashi.ota.dev@gmail.com>
2020-01-13 02:56:22 +0900

    Make project.

---
+++ app/src/main/java/jp/sadashi/manager/password/PasswordDetailFragment.kt
@@ -0,0 +1,34 @@
+package jp.sadashi.manager.password
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+
+class PasswordDetailFragment : Fragment() {
+
+    private val args: PasswordDetailFragmentArgs by navArgs()
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_password_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.textview_second).text = "${args.id}"
+
+        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+            findNavController().navigate(R.id.action_PasswordDetailFragment_pop)
+        }
+    }
+}
