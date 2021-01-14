31988e29dbffba333bc3fd0ef0e57d82f4de9664
Sadashi Ota <sadashi.ota.dev@gmail.com>
2021-01-15 00:32:18 +0900

    追加ボタン実装, ViewBinding実装 (#2)

--- app/src/main/kotlin/jp/sadashi/manager/password/PasswordDetailFragment.kt
+++ app/src/main/kotlin/jp/sadashi/manager/password/PasswordDetailFragment.kt
@@ -4,29 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import jp.sadashi.manager.password.databinding.FragmentPasswordDetailBinding
 
 class PasswordDetailFragment : Fragment() {
 
     private val args: PasswordDetailFragmentArgs by navArgs()
 
+    private var _binding: FragmentPasswordDetailBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_password_detail, container, false)
+    ): View {
+        _binding = FragmentPasswordDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<TextView>(R.id.textview_second).text = "${args.id}"
+        binding.textviewSecond.text = "${args.id}"
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+        binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_PasswordDetailFragment_pop)
         }
     }
